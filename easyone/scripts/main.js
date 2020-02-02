b37671243e00acaf9d84131a42bca6c3739b93dd
18362828885 <46243087+18362828885@users.noreply.github.com>
2020-02-02 22:36:44 +0800

    Add files via upload

---
+++ easyone/scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'imgs/yl.jpg') {
+      myImage.setAttribute('src', 'imgs/fml.jpg');
+    } else {
+      myImage.setAttribute('src', 'imgs/yl.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = myName+'，你酷毙了!' ;;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = storedName+'，你酷毙了!' ;
+}
+
+myButton.onclick = function() {
+   setUserName();
+}
